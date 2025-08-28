8cddaf4cf19d9c2a1729efceaf3ebf2e3a0e8ce3
derekgaleas <64647310+DHK2001@users.noreply.github.com>
2025-08-28 11:16:26 -0600

    implement disable

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,9 +6,10 @@ type Props = {
   disabled?: boolean;
 };
 
-const Button = ({ text, onClick, disabled }: Props) => {
+const Button = ({ text, onClick, disabled=false }: Props) => {
   return (
     <button
+      disabled={disabled}
       onClick={onClick}
       className={`px-4 py-2 bg-[color:var(--primary)] text-[var(--button-text)] shadow-md transition-transform duration-300 ease-in-out hover:shadow-[0_6px_20px_var(--shadow-color)] hover:-translate-y-1  text-base font-bold rounded-md cursor-pointer`}
     >
